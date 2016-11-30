0866c40ed843eba897adabd0eaf5334651fd3f90
Elizabeth Rogers <beth3346@gmail.com>
2016-11-30 07:31:31 -0600

    use es6 syntax

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 # ELR Popovers
 
-[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
-
-A jQuery component
-
-### Dependencies:
-
-+ jQuery
-
-### Features:
-
-### Config:
-
-### Example Usage:
-
-#### Sample html
-
 ###License
 
 ELR-Popovers is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
--- src/elr-popovers.js
+++ src/elr-popovers.js
@@ -1,53 +1,57 @@
-(function($) {
-    window.elrPopovers = function(params) {
-        var self = {};
-        var spec = params || {};
-        var holderClass = spec.holder || 'popover-holder';
-        var $holder = $('.' + holderClass);
-        var popoverClass = spec.popoverClass || 'elr-popover';
+const $ = require('jquery');
 
-        var togglePopover = function() {
-            var $that = $(this);
-            var popoverId = $that.data('popover');
-            var $popover = $('#' + popoverId);
-            
+const elrPopovers = function({
+    holderClass = 'popover-holder',
+    popoverClass = 'elr-popover'
+} = {}) {
+    const $holder = $(`.${holderClass}`);
+    const self = {
+        checkPosition() {
+            // reposition popover if its too close to the edge of the browser window
+            // TODO: need to add right position
+            const $that = $(this);
+            const positionLeft = $that.position().left;
+            const offsetLeft = $that.offset().left;
+            const positionTop = $that.position().top;
+            const offsetTop = $that.offset().top;
+            const popoverHeight = $that.height();
+
+            if (offsetLeft < 0) {
+                $that.css('left', (Math.abs(offsetLeft) + 10) + positionLeft);
+            } else if (offsetTop < 0) {
+                $that.css('bottom', (Math.abs(positionTop) - popoverHeight) - Math.abs(offsetTop));
+            }
+        },
+        togglePopover() {
+            const $that = $(this);
+            const popoverId = $that.data('popover');
+            const $popover = $(`#${popoverId}`);
+
             // fade out any visible popovers
-            $('.' + popoverClass).not('#' + popoverId).not(':hidden').fadeOut();
+            $(`.${popoverClass}`).not(`#${popoverId}`).not(':hidden').fadeOut();
             $popover.fadeToggle();
 
-            var checkPosition = function() {
-                // reposition popover if its too close to the edge of the browser window
-                var $that = $(this);
-                var positionLeft = $that.position().left;
-                var offsetLeft = $that.offset().left;
-                var positionTop = $that.position().top;
-                var offsetTop = $that.offset().top;
-                var popoverHeight = $that.height();
-
-                if ( offsetLeft < 0 ) {
-                    $that.css('left', (Math.abs(offsetLeft) + 10) + positionLeft);
-                } else if ( offsetTop < 0 ) {
-                    $that.css('bottom', (Math.abs(positionTop) - popoverHeight) - Math.abs(offsetTop));
-                }
-            };
-
-            checkPosition.call($popover);
-        };
-
-        if ( $holder.length ) {
-            $buttons = $holder.find('button');
-
-            $('body').on('click', '.' + holderClass + ' button', function(e) {
-                togglePopover.call(this);
-                e.stopPropagation();
-                e.preventDefault();
-            });
-
-            $('body').on('click', function() {
-                $holder.find('.' + popoverClass).fadeOut();
-            });
+            self.checkPosition.call($popover);
         }
-
-        return self;
     };
-})(jQuery);
+
+    if ($holder.length) {
+        const $buttons = $holder.find('button');
+        const $body = $('body');
+
+        $body.on('click', `.${holderClass} button`, function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            self.togglePopover.call(this);
+        });
+
+        $body.on('click', function() {
+            $holder.find(`.${popoverClass}`).fadeOut();
+        });
+    }
+
+    return self;
+};
+
+export default elrPopovers;
